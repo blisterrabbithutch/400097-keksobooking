308f74e702b51a721e3b0eabbc29fb1dcb239e6d
Denis Chizh <rabbit-hutch@ip-192-168-0-100.is74.loc>
2017-09-18 15:10:00 +0500

    правки

---
+++ js/backend.js
@@ -0,0 +1,46 @@
+// backend.js
+(function () {
+  var URLSave = 'https://1510.dump.academy/keksobooking';
+  var URLLoad = 'https://1510.dump.academy/keksobooking/data';
+  // функция отправки данных на сервер
+  // data = объект с данными, котрые необходим отправить data
+  // onSuccess = функция обратного вызова onSuccess, которая будет вызываться каждый раз когда данные отправлены успешно
+  window.xhrTransfer= function (data, onSuccess) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    
+    xhr.addEventListener('load', function () {
+      onSuccess(xhr.response);
+    });
+    // зачем здесь в демке написано else в котором написано onError, если это описывается дальше?
+    xhr.addEventListener('error', function () {
+      onError('Ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+    xhr.timeout = 10000; // 10s
+
+    return xhr;
+  };
+  };
+  window.backend = {
+    load: function (onLoad, onError) {
+      var xhr = xhrTransfer(onSuccess, onError);
+
+      xhr.open('GET', URLLoad);
+      xhr.send();
+    },
+
+    save: function (data, onLoad, onError) {
+      // что за xhrTransfer?? var xhrTransfer = function (onSuccess, onError). зачем дважды дублировать параметры?
+      var xhr = xhrTransfer(onSuccess, onError);
+
+      xhr.open('POST', URLSave);
+      xhr.send(data);
+    },
+
+  }
+  
+
+})();
